0ae55a502992efdbc830de3a751d5451c8ebc2ca
Denis Filippov <dfilippov@oneincsystems.com>
2018-10-02 17:44:50 +0700

    For clr build testing

---
+++ TestCLR/SqlStoredProcedure2.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public partial class StoredProcedures
+{
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static void SqlStoredProcedure2()
+    {
+        SqlConnection sqlConnection = new SqlConnection();
+        SqlCommand sqlCommand = new SqlCommand();
+        try
+        {
+            sqlConnection = new SqlConnection("context connection=true");
+            sqlConnection.Open();
+            sqlCommand = new SqlCommand("INSERT INTO [dbo].[ParamTable] ([SourceFile]) VALUES('MySourceFile2')", sqlConnection);
+            SqlContext.Pipe.ExecuteAndSend(sqlCommand);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        finally
+        {
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+            sqlCommand.Dispose();
+        }
+    }
+}
